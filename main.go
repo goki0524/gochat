f3491963d499e677f64dd8366ac2c779b2d75c19
goki sakaguchi <goki0524@gmail.com>
2018-08-26 00:05:32 +0900

    finish chapter1

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// チャットルーム開始
+	go r.run()
 	// Webサーバーを開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
