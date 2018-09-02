94bb9155b2081641ec4898babc9ad0e3a1d19117
goki sakaguchi <goki0524@gmail.com>
2018-09-02 17:02:50 +0900

    finish 2.2

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 	r.tracer = trace.New(os.Stdout)
 	// http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	// チャットルーム開始
 	go r.run()
