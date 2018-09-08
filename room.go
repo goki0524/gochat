3b27206af2d4f6330e73eca188e5b0ce2eb183b7
goki sakaguchi <goki0524@gmail.com>
2018-09-08 14:19:43 +0900

    finish 3.2
    Added Gravatar function.

--- room.go
+++ room.go
@@ -21,15 +21,19 @@ type room struct {
 	clients map[*client]bool
 	// tracerはチャットルーム上で行われた操作ログを受け取る
 	tracer trace.Tracer
+	// avatarはアバターの情報を取得する
+	avatar Avatar
 }
 
 // newRoomはすぐに利用できるチャットルームを生成して返す
-func newRoom() *room {
+func newRoom(avatar Avatar) *room {
 	return &room{
 		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+		avatar:  avatar,
 	}
 }
 
